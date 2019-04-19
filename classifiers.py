6072c394d9becc05c3132e795e6bd9eb00101291
Gilberto Antunes <gamjun_@hotmail.com>
2019-04-19 20:15:47 -0300

    Last Code Version

--- classifiers.py
+++ classifiers.py
@@ -1,3 +1,5 @@
+import inspect
+
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -24,6 +26,11 @@ def redo_ds_partitions():
 
     train_x, aux_x, train_y, aux_y = train_test_split(x, y, test_size=0.5)
     validation_x, test_x, validation_y, test_y = train_test_split(aux_x, aux_y, test_size=0.5)
+
+
+# função para medir a acurácia da predição
+def accuracy(y_true: object, y_pred: object) -> object:
+    return np.mean(np.equal(y_true, y_pred))
 
 
 # usando um classificador dt
@@ -62,6 +69,144 @@ def svm(bp_svm):
     return accuracy(test_y, supVectorMachine.predict(test_x))
 
 
-# função para medir a acurácia da predição
-def accuracy(y_true: object, y_pred: object) -> object:
-    return np.mean(np.equal(y_true, y_pred))
+# Encontrando melhores parametros=======================================================================================
+def fbp_dt():
+    print('Decision Tree\nCalculando profundidade\t')
+    aux_depth = 0
+    aux_accuracy = 0.00
+
+    decisionTree = DecisionTreeClassifier(max_depth=None)
+    decisionTree = decisionTree.fit(train_x, train_y)
+    if (accuracy(validation_y, decisionTree.predict(validation_x)) > aux_accuracy):
+        aux_accuracy = accuracy(validation_y, decisionTree.predict(validation_x))
+        aux_depth = None
+    for depth in range(1, 10):
+        decisionTree = DecisionTreeClassifier(max_depth=depth)
+        decisionTree = decisionTree.fit(train_x, train_y)
+        if (accuracy(validation_y, decisionTree.predict(validation_x)) > aux_accuracy):
+            aux_accuracy = accuracy(validation_y, decisionTree.predict(validation_x))
+            aux_depth = depth
+    print('Profundidade máxima = ', aux_depth)
+    return (aux_depth)
+
+
+def OneMinusNormDist(distances):
+    ueiguitis = [None] * len(distances)
+    for i in range(0, len(distances)):
+        ueiguitis[i] = (1 - (1 / distances[i]))
+    return ueiguitis
+
+
+# usando um classificador KNN k=1-3-5-7-9-11-13-15 * (não ponderado, ponderado pelo inverso da distância, ponderado por 1-distância normalizada)
+def fbp_knn():
+    print('\nK Nearest Neighbors\nCalculando K, Métrica de Distância\t')
+    aux_accuracy = 0.00
+    aux_weights = ''
+    aux_k = 0
+
+    for k in range(1, 16, 2):
+        kNearestNeighbors_OneMinusNormDist = KNeighborsClassifier(n_neighbors=k, algorithm='auto',
+                                                                  weights=OneMinusNormDist)
+        kNearestNeighbors_OneMinusNormDist.fit(train_x, train_y)
+        if (accuracy(validation_y, kNearestNeighbors_OneMinusNormDist.predict(validation_x)) > aux_accuracy):
+            aux_accuracy = accuracy(validation_y, kNearestNeighbors_OneMinusNormDist.predict(validation_x))
+            aux_weights = OneMinusNormDist
+            aux_k = k
+
+        kNearestNeighbors_uniform = KNeighborsClassifier(n_neighbors=k, algorithm='auto', weights='uniform')
+        kNearestNeighbors_uniform.fit(train_x, train_y)
+        if (accuracy(validation_y, kNearestNeighbors_uniform.predict(validation_x)) > aux_accuracy):
+            # print('swap to uniform')
+            aux_accuracy = accuracy(validation_y, kNearestNeighbors_uniform.predict(validation_x))
+            aux_weights = 'uniform'
+            aux_k = k
+
+        kNearestNeighbors_distance = KNeighborsClassifier(n_neighbors=k, algorithm='auto',
+                                                          weights='distance')
+        kNearestNeighbors_distance.fit(train_x, train_y)
+        if (accuracy(validation_y, kNearestNeighbors_distance.predict(validation_x)) > aux_accuracy):
+            # print('swap to distance')
+            aux_accuracy = accuracy(validation_y, kNearestNeighbors_distance.predict(validation_x))
+            aux_weights = 'distance'
+            aux_k = k
+            # print(accuracy(validation_y, kNearestNeighbors_uniform.predict(validation_x)),
+            #       accuracy(validation_y, kNearestNeighbors_distance.predict(validation_x)))
+
+    print('K = ', aux_k, 'Métrica de Distância = ',
+          '1-Distancia Normalizada' if inspect.isfunction(aux_weights) else aux_weights)
+    return (aux_weights, aux_k)
+
+
+# usando um classificador mlp
+def fbp_mlp():
+    print(
+        '\nMulti Layer Perceptron\nCalculando Número de épocas de treino, taxa de aprendizagem, número de camadas escondidas\t')
+    aux_accuracy = 0.00
+    aux_hls = 0
+    aux_mi = 0
+    aux_learning_rate = ''
+
+    for mi in range(50, 301, 25):
+        for hls in range(1, 15):
+            # print(_, mi, hls)
+
+            multiLayerPerceptron_constant = MLPClassifier(hidden_layer_sizes=hls, activation='relu', max_iter=mi,
+                                                          learning_rate='constant')
+            multiLayerPerceptron_constant.fit(train_x, train_y)
+
+            multiLayerPerceptron_invscaling = MLPClassifier(hidden_layer_sizes=hls, activation='relu', max_iter=mi,
+                                                            learning_rate='invscaling')
+            multiLayerPerceptron_invscaling.fit(train_x, train_y)
+
+            multiLayerPerceptron_adaptive = MLPClassifier(hidden_layer_sizes=hls, activation='relu', max_iter=mi,
+                                                          learning_rate='adaptive')
+            multiLayerPerceptron_adaptive.fit(train_x, train_y)
+
+            if (accuracy(validation_y, multiLayerPerceptron_constant.predict(validation_x)) > aux_accuracy):
+                aux_accuracy = accuracy(validation_y, multiLayerPerceptron_constant.predict(validation_x))
+                aux_learning_rate = 'constant'
+                aux_hls = hls
+                aux_mi = mi
+
+            if (accuracy(validation_y, multiLayerPerceptron_invscaling.predict(validation_x)) > aux_accuracy):
+                aux_accuracy = accuracy(validation_y, multiLayerPerceptron_invscaling.predict(validation_x))
+                aux_learning_rate = 'invscaling'
+                aux_hls = hls
+                aux_mi = mi
+
+            if (accuracy(validation_y, multiLayerPerceptron_adaptive.predict(validation_x)) > aux_accuracy):
+                aux_accuracy = accuracy(validation_y, multiLayerPerceptron_adaptive.predict(validation_x))
+                aux_learning_rate = 'adaptive'
+                aux_hls = hls
+                aux_mi = mi
+
+    print('Número de épocas de treino = ', aux_mi, 'Taxa de aprendizagem = ', aux_learning_rate,
+          'Número de camadas escondidas = ', aux_hls)
+    return (aux_mi, aux_hls, aux_learning_rate)
+
+
+# usando um classificador svm
+def fbp_svm():
+    print('\nSupport Vector Machine\nCalculando Kernel e Penalidade\t')
+    aux_accuracy = 0.00
+    aux_penalty = 0.00
+    aux_kernel = ''
+
+    for penalty in range(1, 51):
+        supVectorMachine_poly = SVC(C=penalty / 10, kernel='poly')
+        supVectorMachine_poly.fit(train_x, train_y)
+
+        supVectorMachine_rbf = SVC(C=penalty / 10, kernel='rbf')
+        supVectorMachine_rbf.fit(train_x, train_y)
+
+        if (accuracy(validation_y, supVectorMachine_poly.predict(validation_x)) > aux_accuracy):
+            aux_accuracy = accuracy(validation_y, supVectorMachine_poly.predict(validation_x))
+            aux_kernel = 'poly'
+            aux_penalty = penalty / 10
+
+        if (accuracy(validation_y, supVectorMachine_rbf.predict(validation_x)) > aux_accuracy):
+            aux_accuracy = accuracy(validation_y, supVectorMachine_rbf.predict(validation_x))
+            aux_kernel = 'rbf'
+            aux_penalty = penalty / 10
+    print('Kernel = ', aux_kernel, 'Penalidade = ', aux_penalty)
+    return (aux_penalty, aux_kernel)
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import inspect
+import statistics
+
 import classifiers
-import find_best_params
 import numpy as np
 from warnings import filterwarnings
+
 filterwarnings('ignore')
 
 dt = []
@@ -9,29 +12,41 @@ knn = []
 mlp = []
 nb = []
 svm = []
-
-print('Calculando Melhores Parametros\n')
-bp_dt = find_best_params.dt()
-bp_knn = find_best_params.knn()
-bp_mlp = find_best_params.mlp()
-print('Naive Bayes\nUsando Configuração Padrão')
-bp_svm = find_best_params.svm()
+params_dt = []
+params_knn = []
+params_mlp = []
+params_svm = []
 
 for index in range(20):
+    print('\n===Iteração ', index, '===\n')
     classifiers.redo_ds_partitions()
+
+    print('Calculando Melhores Parametros\n')
+    bp_dt = classifiers.fbp_dt()
+    params_dt.append(bp_dt)
+    bp_knn = classifiers.fbp_knn()
+    params_knn.append(bp_knn)
+    bp_mlp = classifiers.fbp_mlp()
+    params_mlp.append(bp_mlp)
+    print('Naive Bayes\nUsando Configuração Padrão')
+    bp_svm = classifiers.fbp_svm()
+    params_svm.append(bp_svm)
+
     dt.append(classifiers.dt(bp_dt))
-    print('dt ', index)
     knn.append(classifiers.knn(bp_knn))
-    print('knn', index)
     mlp.append(classifiers.mlp(bp_mlp))
-    print('mlp', index)
     nb.append(classifiers.nb())
-    print('nb', index)
     svm.append(classifiers.svm(bp_svm))
-    print('svm', index)
 
 with open("results.csv", "w") as fp:
-    fp.write("Dt, Knn, Mlp, Nb, Svm\n")
+    fp.write(
+        "Dt, Depth, Knn, Weights, K, Mlp, Max Ite, Hidden Layers, Learning Rate, Nb, No Params, Svm, Kernel, Penalty\n")
     for index in range(20):
-        fp.write("%f, %f, %f, %f, %f\n" % (dt[index], knn[index], mlp[index], nb[index], svm[index]))
-    fp.write("\n%f, %f, %f, %f, %f\n" % (np.mean(dt), np.mean(knn), np.mean(mlp), np.mean(nb), np.mean(svm)))
+        fp.write("%f, %s, %f, %s, %s, %f, %s, %s, %s, %f, %s, %f, %s,%s\n" % (
+            dt[index], params_dt[index], knn[index],'1-Distancia Normalizada' if inspect.isfunction(params_knn[index][0]) else params_knn[index][0], params_knn[index][1], mlp[index],
+            params_mlp[index][0], params_mlp[index][1], params_mlp[index][2], nb[index], "Default", svm[index],
+            params_svm[index][0], params_svm[index][1]))
+    fp.write("\n%f, %s, %f, %s, %s, %f, %s, %s, %s, %f, %s, %f, %s,%s\n" % (
+        np.mean(dt), '', np.mean(knn), '', '', np.mean(mlp), '', '', '', np.mean(nb), '', np.mean(svm), '', ''))
+    fp.write("%f, %s, %f, %s, %s, %f, %s, %s, %s, %f, %s, %f, %s,%s\n" % (
+        statistics.pstdev(dt), '', statistics.pstdev(knn), '', '', statistics.pstdev(mlp), '', '', '', statistics.pstdev(nb), '', statistics.pstdev(svm), '', ''))
